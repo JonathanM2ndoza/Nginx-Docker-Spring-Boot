fe6a48295d96bb518516ba54efaff4ea5a24a41b
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-07-04 18:35:43 -0400

    Load multiple configuration files

---
+++ configuration/src/main/java/com/jmendoza/wallet/configuration/properties/LoadProperties.java
@@ -0,0 +1,13 @@
+package com.jmendoza.wallet.configuration.properties;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:application.properties"),
+        @PropertySource("classpath:message_customer.properties")
+})
+public class LoadProperties {
+}
