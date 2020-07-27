f2e4f8e9f9e95edad3591cde2ef9b7df84d87579
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-07-27 00:58:41 -0400

    Integration with Spring REST Docs

---
+++ configuration/src/test/java/com/jmendoza/wallet/configuration/ConfigurationApplicationTest.java
@@ -0,0 +1,13 @@
+package com.jmendoza.wallet.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationApplicationTest {
+
+	@Test
+	void contextLoads() throws Exception {
+	}
+
+}
