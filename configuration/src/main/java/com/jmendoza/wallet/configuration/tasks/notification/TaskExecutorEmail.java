216ba755837543c2b67bdeca0515fed25634d974
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-08-15 12:23:28 -0400

    Update layer configuration: Adding spring-boot-starter-mail + ThreadPoolTaskExecutor

---
+++ configuration/src/main/java/com/jmendoza/wallet/configuration/tasks/notification/TaskExecutorEmail.java
@@ -0,0 +1,33 @@
+package com.jmendoza.wallet.configuration.tasks.notification;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class TaskExecutorEmail {
+
+    @Autowired
+    private Environment env;
+
+    private static final Logger loggerException = LogManager.getLogger(TaskExecutorEmail.class);
+
+    @Bean(name = "emailExecutor")
+    public TaskExecutor emailExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setThreadNamePrefix(env.getRequiredProperty("task.executor.email.threadNamePrefix"));
+        threadPoolTaskExecutor.setCorePoolSize(Integer.parseInt(env.getRequiredProperty("task.executor.email.corePoolSize")));
+        threadPoolTaskExecutor.setMaxPoolSize(Integer.parseInt(env.getRequiredProperty("task.executor.email.maxPoolSize")));
+        threadPoolTaskExecutor.setQueueCapacity(Integer.parseInt(env.getRequiredProperty("task.executor.email.queueCapacity")));
+        threadPoolTaskExecutor.afterPropertiesSet();
+        loggerException.debug("SET - " + env.getRequiredProperty("task.executor.email.threadNamePrefix"));
+        return threadPoolTaskExecutor;
+    }
+}
