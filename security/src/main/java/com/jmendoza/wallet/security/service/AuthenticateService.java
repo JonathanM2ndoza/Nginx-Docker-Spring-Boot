adaaa9d3f72bd548c88b5d291cbb206db13c8ac1
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-08-19 23:30:23 -0400

    Update layer security: Adding endpoint Refresh Token

--- security/src/main/java/com/jmendoza/wallet/security/service/AuthenticateService.java
+++ security/src/main/java/com/jmendoza/wallet/security/service/AuthenticateService.java
@@ -3,6 +3,7 @@ package com.jmendoza.wallet.security.service;
 import com.jmendoza.wallet.common.exception.GlobalException;
 import com.jmendoza.wallet.security.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,7 +17,11 @@ public class AuthenticateService {
     @Autowired
     private AuthenticationManager authenticationManager;
     @Autowired
+    SecurityUserDetailsService securityUserDetailsService;
+    @Autowired
     private JwtUtil jwtUtil;
+    @Autowired
+    private Environment env;
 
     public String createToken(String email, String password) throws GlobalException {
 
@@ -34,5 +39,20 @@ public class AuthenticateService {
         } catch (Exception e) {
             throw new GlobalException("createToken: " + e.getMessage());
         }
+    }
+
+    public String refreshToken(String email, String encryptedHash) throws GlobalException {
+        String token = null;
+        try {
+            UserDetails userDetails = securityUserDetailsService.loadUserByUsername(email);
+            if (userDetails != null && env.getRequiredProperty("security.jwt.refresh-token").equals(encryptedHash))
+                token = jwtUtil.generateToken(userDetails);
+            else
+                throw new GlobalException("No match refreshToken");
+
+        } catch (Exception e) {
+            throw new GlobalException("tokenRefresh: " + e.getMessage());
+        }
+        return token;
     }
 }
