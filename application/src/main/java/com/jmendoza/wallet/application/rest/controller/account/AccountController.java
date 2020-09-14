cfd3fa05d7a1f254ed7c595660bbaa7d4519a7a7
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-09-14 16:18:23 -0400

    Update layer application: Log4j2 - Add Context information in ThreadContext

--- application/src/main/java/com/jmendoza/wallet/application/rest/controller/account/AccountController.java
+++ application/src/main/java/com/jmendoza/wallet/application/rest/controller/account/AccountController.java
@@ -1,8 +1,11 @@
 package com.jmendoza.wallet.application.rest.controller.account;
 
 import com.jmendoza.wallet.application.rest.response.account.CreateAccountResponse;
+import com.jmendoza.wallet.common.constants.authorization.AuthorizationConstanst;
 import com.jmendoza.wallet.common.exception.GlobalException;
 import com.jmendoza.wallet.common.exception.ParameterNotFoundException;
+import com.jmendoza.wallet.common.log.LogContext;
+import com.jmendoza.wallet.common.log.LogFacade;
 import com.jmendoza.wallet.domain.model.account.Account;
 import com.jmendoza.wallet.domain.ports.inbound.account.CreateAccountUseCase;
 import com.jmendoza.wallet.domain.ports.inbound.account.GetAccountUseCase;
@@ -11,6 +14,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/v1/accounts")
@@ -21,10 +26,19 @@ public class AccountController {
     private final GetAccountUseCase getAccountUseCase;
     private final GetTransactionsByAccountIdUseCase getTransactionsByAccountIdUseCase;
 
+    private static final LogFacade log = new LogFacade(AccountController.class);
+
     @PostMapping
     @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<CreateAccountResponse> createAccount(@RequestBody Account account) throws ParameterNotFoundException, GlobalException {
+    public ResponseEntity<CreateAccountResponse> createAccount(@RequestBody Account account, HttpServletRequest request) throws ParameterNotFoundException, GlobalException {
+        LogContext logContext = new LogContext(request.getRemoteAddr(), "Create Account", String.valueOf(request.getAttribute(AuthorizationConstanst.USER)), "createAccount");
+        logContext.setWildcard1("Test Wildcard1");
+        logContext.setWildcard2("Test Wildcard2");
+        logContext.setWildcard3("Test Wildcard3");
+
+        log.info(logContext, "Test Message");
         createAccountUseCase.createAccount(account);
+
         return ResponseEntity.ok().body(CreateAccountResponse.builder().accountId(account.getAccountId()).build());
     }
 
